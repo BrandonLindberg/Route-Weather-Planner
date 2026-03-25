2e282217361bf36dfd3d735455a8c87d8315e059
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2026-03-24 19:37:54 -0600

    content security policy updates

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ app.use(express.json());
 // Security Headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline';");
+    res.setHeader("Content-Security-Policy",
+        "default-src 'self'; " +
+        "script-src 'self'; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "img-src 'self' data: https://*.tile.openstreetmap.org https://server.arcgisonline.com https://tilecache.rainviewer.com; " +
+        "connect-src 'self' https://api.rainviewer.com;"
+    );
     next();
 })
 
